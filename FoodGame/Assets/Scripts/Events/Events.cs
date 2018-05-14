e24e26f5381a9d41fd0dafe8b74ea747875f5ddd
Jordan <jordantjedejong@hotmail.com>
2018-05-14 16:25:02 +0200

    MainMenu/Events
    mainmenu play button en events SO basic

---
+++ FoodGame/Assets/Scripts/Events/Events.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Analytics;
+
+namespace Events
+{
+    [CreateAssetMenu]
+
+    public class Events : ScriptableObject
+    {
+        public string Headline = "This is a Headline!";
+        public string Content = "This is supposed to be the content of the event";
+        public int Month = 3;
+        public int Year = 1;
+        // happenAt month/year
+        private Vector2 happensAt;
+
+
+        public void OnButtonPressed()
+        {
+            happensAt = new Vector2(Month, Year);
+        }
+    }
+}
---
+++ FoodGame/Assets/Scripts/MainMenu/SceneButton.cs
@@ -0,0 +1,26 @@
+﻿using UI;
+using UnityEngine.SceneManagement;
+
+namespace MainMenu
+{
+    public class SceneButton : UIButtonAbstract
+    {
+
+        // Use this for initialization
+        void Start ()
+        {
+		
+        }
+	
+        // Update is called once per frame
+        void Update ()
+        {
+		
+        }
+
+        public override void OnButtonClick()
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
