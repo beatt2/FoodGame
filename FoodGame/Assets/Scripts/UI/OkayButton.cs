ba4b08bb32d8dbd2592d924982e92915b53f5539
Jordan <jordantjedejong@hotmail.com>
2018-05-17 14:29:06 +0200

    Changed Button onclick

--- FoodGame/Assets/Scripts/UI/OkayButton.cs
+++ FoodGame/Assets/Scripts/UI/OkayButton.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 namespace UI
 {
-    public class OkayButton : UIButtonAbstract
+    public class OkayButton : MonoBehaviour
     {
 
         public GameObject Ui;
-        
-        public override void TaskOnClick()
+
+        //public override void TaskOnClick()
+        //{
+        //    EventManager.Instance.InEventMenu = false;
+        //    Ui.SetActive(false);
+        //}
+        public void CloseFinance()
         {
             EventManager.Instance.InEventMenu = false;
             Ui.SetActive(false);
         }
+
     }
 }
