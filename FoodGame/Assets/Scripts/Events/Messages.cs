8a2ce96cf826e072a6afed4ba0c73586bc2665a2
Unknown <koengroten@gmail.com>
2018-06-12 21:18:59 +0200

    Fixed messages

--- FoodGame/Assets/Scripts/Events/Messages.cs
+++ FoodGame/Assets/Scripts/Events/Messages.cs
@@ -12,6 +12,8 @@ namespace Events
         public GameObject Content;
         private RectTransform _currentpos;
         public RectTransform StartingPos;
+        private Vector3 _startingPos;
+        private RectTransform _rectTransform;
 
         private readonly List<GameObject> _go = new List<GameObject>();
         private readonly List<MessageEntry> _messageText = new List<MessageEntry>();
@@ -30,6 +32,9 @@ namespace Events
         void Start ()
         {
             _currentpos = StartingPos;
+            _startingPos = StartingPos.position;
+            _rectTransform = Content.GetComponent<RectTransform>();
+
 
             //Add("testheadline","Test content","Test effect", 1);
 
@@ -74,45 +79,51 @@ namespace Events
         public void AddEvent(Events events)
         {
             _eventsInInbox.Add(events);
-             
-            
+
+
             string effect = GetName(events.FieldType) + " " + events.InfluencePercentage + "%";
             Add(events.Headline,events.Content, effect);
         }
 
         public void Add(string headline, string content, string effect)
         {
-          
+
             _messageText.Add(new MessageEntry(headline, content, effect));
 
-            GameObject go = Instantiate(HeadlineUiPrefab, StartingPos.position, Quaternion.identity, Content.transform) as GameObject;
-            _go.Add(go);
+            GameObject go = Instantiate(HeadlineUiPrefab, _startingPos, Quaternion.identity, Content.transform) as GameObject;
+            _go.Insert(0,go);
 
             if (_go.Count > 1)
             {
                 UpdateTextPos();
             }
+
+            if (_go.Count > 3)
+            {
+
+                _rectTransform.sizeDelta = new Vector2(_rectTransform.sizeDelta.x,_rectTransform.sizeDelta.y - Gap);
+            }
             MessagePrefab messagePrefab = go.GetComponent<MessagePrefab>();
             messagePrefab.Headline.text = headline;
-
-            
-            
             Button tempButton = go.GetComponent<Button>();
             int tempCount = _eventsInInbox.Count;
             tempButton.onClick.AddListener(() => this.OnButtonClick(tempCount));
             //ChangeText(go, headline,content,effect);
         }
 
-        public void UpdateTextPos()
+        private void UpdateTextPos()
         {
-            int multiplierRate = _go.Count;
-            for (int i = 1; i < _go.Count; i++ , multiplierRate--)
+            for (int i = 1; i < _go.Count; i++)
             {
-                _currentpos.position = new Vector3(_currentpos.position.x, _currentpos.position.y + Gap,_currentpos.position.z);
+                float tempGap = Gap * i;
+                Debug.Log(_startingPos);
+
+                _currentpos.position = new Vector3(_currentpos.position.x, _currentpos.position.y + tempGap,_currentpos.position.z);
                 _go[i].transform.position = _currentpos.position;
+                _currentpos.position = _startingPos;
             }
 
-            _currentpos.position = StartingPos.position;
+
         }
 
         //public void ChangeText(GameObject go,string headline, string content, string effect)
@@ -123,14 +134,14 @@ namespace Events
         //    messagePrefab.Headline.text = headline;
         //    messagePrefab.Content.text = content;
         //    messagePrefab.Effect.text = effect;
-            
+
         //    //Content.headline = messagePrefab.headline
         //    //PopupScript.ContentText = messagePrefab.Content;
         //    //PopupScript.EffectText = messagePrefab.Effect;
-        
-            
-            
-            
+
+
+
+
         //}
 
         public void ChangeTextOnButton(int index)
