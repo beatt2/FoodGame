518dbd9067d4e5b375118362d7c845bc2f881ce1
Unknown <jordantjedejong@hotmail.com>
2018-06-12 14:27:01 +0200

    kanskaarten begin

---
+++ FoodGame/Assets/Scripts/KansKaarten/KanskaartButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KanskaartButtons : MonoBehaviour
+{
+    public GameObject Kanskaart;
+    public void Confirm()
+    {
+        Kanskaart.SetActive(false);
+    }
+
+    public void Decline()
+    {
+        Kanskaart.SetActive(false);
+    }
+}
---
+++ FoodGame/Assets/Scripts/KansKaarten/Kanskaarten.cs
@@ -0,0 +1,21 @@
+﻿using Node;
+using UnityEngine;
+
+namespace KansKaarten
+{
+    [CreateAssetMenu]
+
+
+    public class Kanskaarten : ScriptableObject
+    {
+
+        public string Headline;
+        public Vector2Int Starts;
+
+        public NodeState.FieldTypeEnum FieldType;
+        public NodeState.CurrentStateEnum Type;
+        public float InfluencePercentage;
+        public float Reward;
+
+    }
+}
