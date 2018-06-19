e5ab9d8139a5c549d1f38cb05d86ba5d42d58515
bt2 <koengroten@gmail.com>
2018-06-19 18:32:05 +0200

    Back to desktop

--- FoodGame/Assets/Scripts/UI/ReviewMessages.cs
+++ FoodGame/Assets/Scripts/UI/ReviewMessages.cs
@@ -52,7 +52,7 @@ namespace Events
             GameObject go = Instantiate(HeadlineUiPrefab, _startingPos, Quaternion.identity, Content.transform);
             go.GetComponent<Image>().sprite = percentage < 0 ? EventManager.Instance.ReviewBackgroundNegative.GetRandom_Array() : EventManager.Instance.ReviewBackgroundPositive.GetRandom_Array();
 
-            else if (percentage < 0)
+            if (percentage < 0)
             {
                 if (percentage < 0)
                 {
@@ -75,8 +75,8 @@ namespace Events
             MessagePrefab messagePrefab = go.GetComponent<MessagePrefab>();
             messagePrefab.Headline.text = headline;
             Button tempButton = go.GetComponent<Button>();
-            int tempCount = _eventsInInbox.Count;
-            tempButton.onClick.AddListener(() => this.OnButtonClick(tempCount));
+            int tempCount = _reviewsInInbox.Count;
+            tempButton.onClick.AddListener(() => OnButtonClick(tempCount));
 
 
             if (!_readDict.ContainsKey(tempCount))
@@ -85,5 +85,53 @@ namespace Events
                 Exclamation.SetActive(true);
             }
         }
+        private void UpdateTextPos()
+        {
+            for (int i = 1; i < _go.Count; i++)
+            {
+                float tempGap = Gap * i;
+    
+                _currentpos.position = new Vector3(_currentpos.position.x, _currentpos.position.y + tempGap,
+                    _currentpos.position.z);
+                _go[i].transform.position = _currentpos.position;
+                _currentpos.position = _startingPos;
+            }
+        }
+        
+        public void OnButtonClick(int index)
+        {
+            ChangeTextOnButton(index);
+            _readDict[index] = true;
+            if (ReadDictBool())
+            {
+                Exclamation.SetActive(false);
+            }
+            PopupUi.SetActive(true);
+        }
+        public void ChangeTextOnButton(int index)
+        {
+            Debug.Log(index);
+            MessagePrefab messagePrefab = _go[index - 1].GetComponent<MessagePrefab>();
+            messagePrefab.Content = PopupScript.ContentText;
+            messagePrefab.Effect = PopupScript.EffectText;
+            messagePrefab.Content.text = _reviewsInInbox[index - 1].Content;
+            string effect = Finance.GetName(_eventsInInbox[index - 1].FieldType) + " " +
+                            _eventsInInbox[index - 1].InfluencePercentage + "%";
+            messagePrefab.Effect.text = effect;
+        }
+        
+        private bool ReadDictBool()
+        {
+            for (int i = 0; i < _readDict.Keys.Count; i++)
+            {
+                if (_readDict[_readDict.ElementAt(i).Key] == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
     }
 }
